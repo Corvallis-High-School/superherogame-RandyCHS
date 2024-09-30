5cce8845a8ac4a221ec6d8761389adf6bd84e40a
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2024-09-30 14:56:00 +0000

    Refactor Superhero class: Add missing constructors and methods
    This commit adds missing constructors and methods to the Superhero class. The constructors now initialize the attributes "life" to true and "weaponStrength" to 0. The new methods include "loseLife()" to set the "life" attribute to false, and an overridden "toString()" method to print out the Superhero details.

--- Superhero.java
+++ Superhero.java
@@ -6,13 +6,27 @@ public class Superhero {
   private int weaponStrength;
 
   // TODO #1: Add missing constructors. Note that the life attribute should default to true and the weaponStrength attribute should default to 0.
-
+  public Superhero(String name, String costume, String weapon) {
+    this.name = name;
+    this.costume = costume;
+    this.weapon = weapon;
+    this.life = true; // Initialize life to true
+    this.weaponStrength = 0; // Initialize weaponStrength to 0
+  }
   // TODO #2: Add missing methods.
+  public void loseLife() {
+    this.life = false;
+  }
   
   // TODO #3: Including a toString() method (see 2.7) to help with printing out the Superhero details.
-  public String toString(){
-  // all your code below to create a string with details
-    String superHeroDetails = "";
-    return superHeroDetails;
+  @Override
+  public String toString() {
+    return "Superhero{" +
+            "name='" + name + '\'' +
+            ", costume='" + costume + '\'' +
+            ", weapon='" + weapon + '\'' +
+            ", life=" + life +
+            ", weaponStrength=" + weaponStrength +
+            '}';
   }
 }
